b22fccd5d7e7014cd2bf02ba5645e40c9f6c54e0
Ivan Bulakhov <kabarchonok@gmail.com>
2024-09-10 17:00:45 +0300

    feat(typescript): add d.ts support

--- packages/trackula/vite.config.ts
+++ packages/trackula/vite.config.ts
@@ -1,14 +1,22 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
 import packageJson from './package.json'
 
 export default defineConfig({
   build: {
     sourcemap: true,
+    minify: false,
+
     lib: {
       entry: resolve(__dirname, './index.ts'),
       name: packageJson.name,
       formats: ['es', 'cjs'],
     },
   },
+
+  plugins: [
+    dts({ rollupTypes: true, tsconfigPath: './tsconfig.json' }),
+  ],
 })
