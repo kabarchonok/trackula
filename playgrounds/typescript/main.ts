d57296f00fdfb1368ec53a4af3df98da94f985ea
Ivan Bulakhov <kabarchonok@gmail.com>
2024-09-17 14:58:49 +0300

    docs(playground): add TypeScript playground

--- playgrounds/typescript/main.ts
+++ playgrounds/typescript/main.ts
@@ -1,4 +1,5 @@
 import _trackula from 'trackula'
+import type { TrackulaInput } from 'trackula'
 
 const $app = document.getElementById('app')
 const $hint = document.getElementById('hint')
@@ -6,6 +7,6 @@ const $hint = document.getElementById('hint')
 const trackula = _trackula({ root: $app, subscribe: onChange })
 trackula.init()
 
-function onChange(inputType) {
+function onChange(inputType: TrackulaInput) {
   $hint.textContent = `You are now using a ${inputType}`
 }
